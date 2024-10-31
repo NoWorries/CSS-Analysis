73a907716d5299b1d790113882a693b4dd6d85b9
Josh Harwood <josh@noworries.nz>
2024-11-01 11:22:36 +1300

    padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CSSAnalyzer from "./CSSAnalyzer";
 
 function App() {
   return (
-    <><div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <><div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-8">
       <CSSAnalyzer /></div>
     </>
   );
